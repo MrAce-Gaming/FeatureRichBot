ccc73438c2d39cfecfefd93fc8f03b68288082d5
code-charm-linux <139982785+MrAce-Gaming@users.noreply.github.com>
2024-06-29 18:31:32 +0530

    Added Moderation Commands
    - AutoMod Filter
    + Ban Command
    + Kick Command

--- bot.py
+++ bot.py
@@ -1,36 +1,9 @@
 import discord
 import config
-import aiohttp
-import time
 from discord.ext import commands
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=">", intents=intents)
-async def automodFilter(guild):
-    try:
-        trigger = discord.AutoModTrigger(
-            type=discord.AutoModRuleTriggerType.keyword,
-            keyword_filter=discord.AutoModPresets(
-                sexual_content=True,
-                slurs=True,
-                profanity=True
-            )
-        )
-        action = discord.AutoModRuleAction(
-            type=discord.AutoModRuleActionType.block_message 
-        )
-
-        rule = discord.AutoModRule(
-            id=1,
-            name="Filter",
-            trigger=trigger,
-            enabled=True,
-            event_type=discord.AutoModRuleEventType.message_send,
-            actions=[action] 
-        )
-        await guild.automodFilter(rule)
-    except Exception as e:
-        print(f"Failed to create AutoModRule: {e}")
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -39,18 +12,58 @@ async def on_ready():
 
 @bot.event
 async def on_message(msg: discord.Message):
-    content = msg.content
-    print(content)
+    message = msg.content
+    if "kys" == message:
+        await msg.reply("yeah bro go kill yourself da fuq")
+    if "mrace" == message:
+        await msg.reply("mrace more like mrass")
+    if "scarry" == message:
+        await msg.reply("bro got the most high end pc known to mankind")
+    if "sam" == message:
+        await msg.reply("let HIM COOK!")
+    await bot.process_commands(msg)
 
-    guild = msg.guild
-    if guild:
-        await automodFilter(guild)
-@bot.command()
-async def welcome(ctx):
-    await ctx.send("Thanks!")
+@bot.tree.command()
+async def welcome(interaction:    discord.Interaction):
+    await interaction.response.send_message("Thanks!")
 
 @bot.tree.command()
 async def hello(interaction:    discord.Interaction):
     await interaction.response.send_message("Hello bro!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+@bot.command()
+async def ban(ctx, user:    discord.Member, reason=None):
+    if user is None:
+        await ctx.send("You can't ban no one")
+    else:
+        await ctx.guild.ban(user, reason=reason)
+        await ctx.send(f"{user} was banned, Reason: {reason}")
+
+@bot.command()
+async def unban(ctx, user:  discord.User, reason=None):
+    if user is None:
+        await ctx.send("Are you trying to unban air?")
+    else:
+        await ctx.guild.unban(user, reason=reason)
+        await ctx.send(f"{user} has been unbanned, Reason: {reason}")
+
+@bot.command()
+async def kick(ctx, user:   discord.Member, reason=None):
+    if user is None:
+        await ctx.send("You cant kick no one")
+    else:
+        await ctx.guild.kick(user, reason=reason)
+        await ctx.send(f"{user} was kicked, Reason: {reason}")
+
+@bot.command()
+async def mute(ctx, user:   discord.Member, reason=None):
+    if user is None:
+        await ctx.send("Bro really be trying to mute air")
+    else:
+        await ctx.send("hmm")
 
 bot.run(config.DISCORD_TOKEN)
